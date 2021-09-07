dc6dd9f328323369c22ca1bc8731af6371ccce63
luisEfe80 <luismendez80@hotmail.com>
2021-09-07 08:54:21 -0400

    Adición de contenido

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -46,14 +46,52 @@
     .row.mb-5
       .col-lg-2(data-aos="flip-left")
         figure.mb-5
-          img(src="@/assets/curso/t4-i2.jpg", alt="Mujeres caminando en el lobby del hotel")
+          img(src="@/assets/curso/t4-ic1.svg", alt="icono decorativo")
       .col-lg-10(data-aos="slide-right")
         p.mb-0  En el día a día de la operación hotelera y cada vez que un cliente se acerca al counter de recepción o usa medios escritos o digitales para contactar al hotel, se genera una comunicación y es importante determinar qué tipo de contacto es el que está realizando el cliente, y de acuerdo con esto, manejarlo de la forma adecuada. Tener un sistema consolidado de gestión de PQRS brinda la oportunidad de fortalecer el servicio, mejorar la operatividad y brindar una excelente experiencia de usuario.
 
     figcaption.customCaption.mb-2 <b>Figura</b> Tipos de solicitudes
     .row.mb-5
       .col-lg-4
+        .nav-holder.align-items-center
+          img(src="@/assets/curso/t4-i3.svg", alt="Imagen decorativa")
+          .text.p-lg-3.p-4
+            .row.px-lg-3.mb-lg-2
+              p.h3 Peticiones
+              p Las peticiones son las interacciones verbales, escritas, o digitales, por medio de la cuales un usuario, de manera respetuosa, solicita a la empresa una intervención en un asunto puntual y concreto relacionado con el servicio. La petición puede ser una solicitud o una información.
 
+      .col-lg-4
+        .nav-holder.align-items-center
+          img(src="@/assets/curso/t4-i4.svg", alt="Imagen decorativa")
+          .text.p-lg-3.p-4
+            .row.px-lg-3.mb-lg-2
+              p.h3 Quejas
+              p Son la expresión o manifestación verbal, escrita o digital que hace el usuario a la empresa por inconformidad generada en la prestación del servicio.
+
+      .col-lg-4
+        .nav-holder.align-items-center
+          img(src="@/assets/curso/t4-i5.svg", alt="Imagen decorativa")
+          .text.p-lg-3.p-4
+            .row.px-lg-3.mb-lg-2
+              p.h3 Reclamos
+              p Son una inconformidad o contrariedad presentada por el usuario, referida a la prestación del servicio o producto comercializado, que incumple un derecho en el cual el usuario se siente perjudicado o amenazado, con el objeto de que se revise y evalúe una contraprestación en términos económicos. 
+    
+    .row.mb-5.justify-content-center
+      .col-lg-4    
+        .nav-holder.align-items-center
+          img(src="@/assets/curso/t4-i6.svg", alt="Imagen decorativa")
+          .text.p-lg-3.p-4
+            .row.px-lg-3.mb-lg-2
+              p.h3 Sugerencias
+              p Son expresiones verbales, escritas o digitales en las que el usuario presenta una propuesta para incidir en el mejoramiento de un proceso de la empresa que está relacionado con la prestación del servicio.  
+
+      .col-lg-4    
+        .nav-holder.align-items-center
+          img(src="@/assets/curso/t4-i7.svg", alt="Imagen decorativa")
+          .text.p-lg-3.p-4
+            .row.px-lg-3.mb-lg-2
+              p.h3 Felicitaciones
+              p Son expresiones en las que un usuario manifiesta su gratitud o agrado por un servicio que se le ha prestado y que ha causado una gran impresión en él. Por lo general, las felicitaciones van dirigidas a una persona en particular. 
 
 
     Separador
@@ -61,6 +99,45 @@
     br
     #t_4_3.titulo-segundo
       h2 4.3. Procedimiento de manejo
+    .row.mb-4
+      .col-lg-2(data-aos="slide-left")
+        figure.mb-5
+          img(src="@/assets/curso/t4-ic2.svg", alt="icono decorativo")
+      .col-lg-10(data-aos="slide-right")
+        p.mb-3.text-bold  Hasta ahora, queda clara la importancia del adecuado servicio al cliente y, por ende, la necesidad de la creación y consolidación de un sistema de atención de PQRS eficiente. 
+        p.mb-0  De esta manera, se podrá lograr la continuidad y fortalecimiento de la relación entre empresa (hotel) y cliente y su posible fidelización. Considere el sistema PQRS (Peticiones, Quejas, Reclamos y Sugerencias) como un arte que hay que aprender y dominar.
+
+    p.mb-5  A continuación, se sugieren algunas recomendaciones básicas para lograrlo:
+
+    figcaption.customCaption.mb-3 <b>Figura</b> &nbsp;Recomendaciones
+    .row.mb-5.justify-content-center
+      .col-lg-6
+        ImagenInfografica.color-secundario.mb-5
+          template(v-slot:imagen)
+            figure
+              img(src='@/assets/curso/t4-i8.svg', alt='Texto que describa la imagen')
+
+          .tarjeta.color-secundario.shadow-box.p-3(x="20%" y="20%")
+            p El hotel debe siempre tomar la iniciativa de servir a los prospectos y clientes. En servicio al cliente, es relevante tener el deseo genuino de servir y ayudar, con la mayor calidad posible.
+          .tarjeta.color-secundario.shadow-box.p-3(x="80%" y="20%")
+            p Siempre debe tratar de generar empatía con el cliente. Esto le ayudará, sobre todo, con personas que estén molestas o exaltadas. La empatía implica ponerse en lugar de la otra persona para entenderlo.
+          .tarjeta.color-secundario.shadow-box.p-3(x="92%" y="50%")
+            p Trate de ser resolutivo, el cliente podrá percibir que usted lo quiere ayudar.
+          .tarjeta.color-secundario.shadow-box.p-3(x="80%" y="80%")
+            p Una vez escuche y procese el mensaje del cliente, trate de ser asertivo, es decir, procure dar su respuesta o punto de vista al cliente, considerando el mejor momento, las palabras concretas y el tono más adecuado.
+          .tarjeta.color-secundario.shadow-box.p-3(x="20%" y="80%")
+            p Sea cual sea la situación, lo que el cliente más valora es ser escuchado. Pero esta escucha debe hacerse de forma activa, es decir, debe demostrar al cliente que usted realmente está interesado en lo que le dice y confirmar que le ha entendido. 
+          .tarjeta.color-secundario.shadow-box.p-3(x="9%" y="50%")
+            p Entender al cliente como lo que es: un ser humano, que tiene deseos, expectativas y aspiraciones. Los hoteles deben propender por conocerlos y, en la medida de lo posible, anticiparse a ellos.
+
+
+
+
+    p.mb-5  La administración del hotel, con base en estas recomendaciones, debe diseñar un manual de manejo de las PQRS y brindar entrenamiento al personal, con el objeto de crear y mantener estándares de calidad en el tratamiento de estas y aprender a encontrar en ese manejo oportunidades de mejora.
+    .cajon.color-secundario.p-4(data-aos="flip-up")
+      p.mb-0 En esta época, cuando los datos son una fuente importante de elementos de acción, los departamentos de atención deben producir informes detallados de la gestión; es por eso importante que el sistema de PQRS esté basado y soportado por el sistema PMS, a razón de registrar, controlar y utilizar esta información.
+
+
 
 
     Separador
@@ -68,6 +145,42 @@
     br
     #t_4_4.titulo-segundo
       h2 4.4 Manual de PQRS
+    p.mb-5.text-bold  Aunque la elaboración del manual de PQRS es un tema interno de cada organización, a continuación, se estudiarán algunas normas básicas que contribuyen con este proceso.
+    .row
+      .col-lg-3
+        figure.mb-5
+          img(src="@/assets/curso/t4-i9.svg", alt="icono decorativo")
+      .col-lg-9
+        ul.lista-ul
+          li(data-aos="flip-up")
+            i.fas.fa-check
+            p Fijar un objetivo que, de manera general, trace la línea conductual respecto de la gestión de los PQRS.
+          li(data-aos="flip-up")
+            i.fas.fa-check
+            p Establecer objetivos específicos que ayuden a identificar y solventar estas situaciones.
+          li(data-aos="flip-up")
+            i.fas.fa-check
+            p Incluir la terminología más relevante, a razón de unificar criterios y formas comunicativas.
+          li(data-aos="flip-up")
+            i.fas.fa-check
+            p Introducir una mención a la normatividad vigente.
+          li(data-aos="flip-up")
+            i.fas.fa-check
+            p Determinar cuáles son los canales básicos de recepción y comunicación con los clientes.
+          li(data-aos="flip-up")
+            i.fas.fa-check
+            p Precisar cuáles serán los métodos de registro y retención de la información.
+          li(data-aos="flip-up")
+            i.fas.fa-check
+            p Delimitar el actuar, según el cargo de responsabilidad, es decir, hasta dónde debe y puede llegar un determinado funcionario. Debe haber un responsable principal.
+          li(data-aos="flip-up")
+            i.fas.fa-check
+            p Determinar cuál será la ruta de las PQRS. A manera de ejemplo: recepción, análisis, respuesta, registro, monitoreo y cierre.
+          li(data-aos="flip-up")
+            i.fas.fa-check
+            p Establecer plazos para las respuestas.
+
+
 
 
     Separador
@@ -75,6 +188,16 @@
     br
     #t_4_5.titulo-segundo
       h2 4.5 Manejo de situaciones difíciles
+    p.mb-5  Los hoteles tienen siempre una situación particular en el manejo de PQRS y es que las quejas pueden llegar directamente al mostrador de la recepción; en este escenario, el contacto directo puede hacer que la situación sea de mayor tensión. Si se ve enfrentado a un cliente que quizá esté ofuscado, lo primero es mantener la calma.
+    p.mb-5  A continuación, se ilustra una posible ruta para este manejo.
+    figcaption.customCaption.mb-3 <b>Figura</b> Ruta de manejo
+    .row.mb-5.justify-content-center
+      .col-lg-10(data-aos="flip-left")
+        figure.mb-5
+          img(src="@/assets/curso/t4-i10.svg", alt="proceso de manejo de situaciones dificiles")
+    p.mb-5  Las situaciones difíciles son cotidianas en cualquier área de servicio al cliente, no obstante, prepararse para estas situaciones puede permitir llegar a acuerdos y soluciones con mayor facilidad. No olvide repasar este contenido las veces que sea necesario para que pueda manejarlas de la mejor forma posible.
+    .cajon.color-secundario.p-4(data-aos="slide-left")
+      p Recuerde que el cliente, en su condición de adquirente del servicio, siempre estará evaluando sus acciones y se encargará de comunicarlas a la comunidad de acuerdo con el manejo que usted le dé.
 
 
 
@@ -83,48 +206,6 @@
 
 
 
-
-    Separador
-    br
-    br
-    #t_4_2.titulo-segundo
-      h2 4.2 Tipos de solicitudes
-
-
-
-
-
-
-
-
-
-
-
-    Separador
-    br
-    br
-    #t_4_3.titulo-segundo
-      h2 4.3. Procedimiento de manejo
-
-
-
-
-
-
-
-
-    Separador
-    br
-    br
-    #t_4_4.titulo-segundo
-      h2 4.4 Manual de PQRS
-
-
-    Separador
-    br
-    br
-    #t_4_4.titulo-segundo
-      h2 4.5. Manejo de situaciones difíciles
 
 
 
