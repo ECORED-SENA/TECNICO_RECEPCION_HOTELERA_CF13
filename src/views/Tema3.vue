81f1f87a3581c2109bc10fed4e4d0b2bb9164c02
luisEfe80 <luismendez80@hotmail.com>
2021-09-07 11:08:12 -0400

    Ajuste de contenido e imagenes

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -17,7 +17,7 @@
           p.mb-0  Como resultado, es posible monitorear la actividad crediticia de los huéspedes, proyectar el flujo de efectivo diario de la venta de habitaciones y comparar los ingresos proyectados (presupuesto) y los reales para los diversos departamentos.
       .col-lg-5
         figure.mb-5(data-aos="fade-up")
-          img(src="@/assets/curso/t2-i1.jpg", alt="imagen decorativa")
+          img(src="@/assets/curso/t3-i1.jpg", alt="imagen decorativa")
 
     p.mb-5.text-bold  El proceso de auditoría realiza un seguimiento (control) de los cargos y pagos (débitos y créditos) y de los recibos de soporte de cargos departamentales, a diario. 
     .row.justify-content-center.mb-5
@@ -39,11 +39,68 @@
               .bloque-texto-a__texto.p-4
                 p Esta persona también debe hacer el registro <i>(check-in)</i> y la salida <i>(check-out)</i> de los huéspedes que lleguen después de las 10:00 p.m. o salgan antes de las 6:00 a.m. Adicionalmente, procesar reservas, realizar las tareas de supervisor de seguridad, verificar los sistemas de seguridad contra incendios, actuar como cajero para las funciones de banquetes y realizar el trabajo de gerente de turno. Como se observa, esta posición tiene un alto grado de responsabilidad y es muy importante en el funcionamiento del hotel.
 
+    .row.mb-5
+      .col-lg-2
+        figure.mb-5(data-aos="fade-up")
+          img.nW(src="@/assets/curso/t3-ic1.svg", alt="imagen decorativa")
+      .col-lg-10
+        p.mb-3  Aprender el proceso de auditoría nocturna dará habilidades valiosas para alguien que planea continuar en la industria hotelera. También ofrece la visión general objetiva necesaria para evaluar la actividad financiera del hotel. Desde su rol de aprendiz, en la auditoría nocturna usted podrá tomar conciencia del papel del gerente general, ya que la auditoría nocturna revisa toda la actividad financiera que se lleva a cabo en un hotel en un día.
+        p.mb-0  Con base en esa verificación, un gerente general puede determinar cómo debe adaptarse la operación para cumplir con los objetivos de gastos y beneficios para el periodo contable y ver si los planes de marketing y las actividades comerciales y operativas están marchando con eficiencia.
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        .cajon.color-secundario.p-4(data-aos="flip-up")
+          p Además de ser un cargo de una altísima responsabilidad, para ejercer la auditoría se debe tener vocación de trabajo, ya que el horario nocturno suele traer muchos cambios en la vida de las personas.
 
+
+    Separador
+    br
+    br
 
     #t_3_3.titulo-segundo
       h2 3.3 El proceso técnico
+    .row.mb-3
+      .col-lg-7(data-aos="slide-left")
+        p.mb-3.text-bold  La auditoría nocturna es una de esas actividades que probablemente no tienen mucha visibilidad, pero que son neurálgicas en el funcionamiento hotelero.
+        p.mb-3  Con los informes emitidos en la auditoría, la gerencia controla la exactitud de las cuentas huésped y mide la eficacia operativa. Esta inspección diaria da la capacidad al gerente para controlar los costos y cumplir con los objetivos de rendimiento, por lo tanto, el rigor en el procedimiento es vital. Se recomienda a los futuros hoteleros que quieran comprender la importancia de la auditoría nocturna participar en la preparación de una. A continuación, se detallan los pasos del proceso de revisión, que, si se aplican en el orden indicado, le garantizarán llevar a buen término el procedimiento.
+      .col-lg-5
+        figure.mb-5(data-aos="slide-right")
+          img.nW(src="@/assets/curso/t3-i2.jpg", alt="imagen decorativa")
+    .row.mb-5.justify-content-center
+      .col-lg-12
+        figure.mb-5(data-aos="fade-up")
+          img(src="@/assets/curso/t3-info1.svg", alt="imagen decorativa")
 
+    p.mb-5  El proceso presentado en este módulo describe la realización del procedimiento de auditoría nocturna con base en un sistema de administración de propiedades o PMS.
+    .row
+      .col-lg-4
+        figure.mb-5(data-aos="flip-left")
+          img.nW(src="@/assets/curso/t3-i3.svg", alt="imagen decorativa")
+      .col-lg-8
+        p.mb-3  El auditor nocturno desempeña varias labores, entre ellas las de recepcionista nocturno. En ese sentido, su primera actividad al presentarse a trabajar será recibir el turno de sus compañeros, lo que implica:
+        ul.lista-ul
+          li
+            i.fas.fa-check
+            p Recibir la caja de recepción (fondo de caja).
+          li
+            i.fas.fa-check
+            p Leer y firmar la bitácora de novedades del turno.
+          li
+            i.fas.fa-check
+            p Verificar qué llegadas están pendientes de arribar.
+          
+        p.mb-3  Una vez hecho esto, estará a cargo del turno y lo siguiente será administrar los procesos a realizar de acuerdo con el tiempo disponible. 
+        .cajon.color-secundario.p-4
+          p Por lo general, primero se realizan las labores de recepción y posteriormente se inicia con el proceso de auditoría. Este tiempo lo establece cada hotel, pero por lo general inicia luego de la medianoche, cuando el movimiento es más lento y todos o casi todos los ambientes han cerrado.
+        
+        p.mb-3  El proceso de auditoría consta de dos fases: un primer momento de revisión de información y el segundo, en el que se realiza el proceso de cierre del sistema.
+          
+          
+
+
+
+    Separador
+    br
+    br
     #t_3_4.titulo-segundo
       h2 3.4 Reportes
 
