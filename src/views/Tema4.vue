f734edfa23a6f88e7006a874eb35e13b50ff93d2
luisEfe80 <luismendez80@hotmail.com>
2021-09-07 06:55:43 -0400

    Adicon de tema 4

---
+++ src/views/Tema4.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+    
+    .titulo-principal(data-aos="flip-up")
+      .titulo-principal__numero
+        span 3
+      h1 Auditoría nocturna
+    
+    #t_3_1.titulo-segundo
+      h2 2.1 Infografía Animada
+
+
+
+</template>
+
+<script>
+export default {
+  name: 'Tema3',
+  data: () => ({
+    // variables de vue
+  }),
+  mounted() {
+    this.$nextTick(() => {
+      this.$aosRefresh()
+    })
+  },
+  updated() {
+    this.$aosRefresh()
+  },
+}
+</script>
+
+<style lang="sass" scoped></style>
